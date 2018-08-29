b4d9878978299299130174deec1c3c325cfb3298
Jacek Rzeniewicz <jacek@boclips.com>
2018-08-29 15:56:27 +0100

    [#159792049] minify CSS configure bundle-size limits
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- webpack-config/webpack.prod.js
+++ webpack-config/webpack.prod.js
@@ -1,4 +1,6 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
@@ -7,11 +9,23 @@ const distPath = path.resolve(__dirname, '../dist');
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     performance: {
-        hints: 'error'
+        hints: 'error',
+        maxAssetSize: 400000,
+        maxEntrypointSize: 1000000
     },
     plugins: [
         // Cleans dist folder before building
-        new CleanWebpackPlugin([distPath], { root: path.resolve(__dirname , '..') }),
+        new CleanWebpackPlugin([distPath], {root: path.resolve(__dirname, '..')}),
     ]
 });
