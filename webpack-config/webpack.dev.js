43dc0036001bda756bec14828afca3b6d8f619d3
José Carlos Valero Sánchez <josecarlosvalerosanchez@gmail.com>
2018-09-13 16:07:45 +0200

    Set API as constants

--- webpack-config/webpack.dev.js
+++ webpack-config/webpack.dev.js
@@ -1,16 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const stagingUrl = 'https://video-service.staging-boclips.com/';
+const localUrl = 'http://localhost:8080/';
+
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    devServer: {
-        historyApiFallback: true,
-        proxy: {
-            '/v1/**': {
-              target: 'https://video-service.staging-boclips.com/',
-                changeOrigin: true,
-            },
-        },
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/v1/**': {
+        target: stagingUrl,
+        changeOrigin: true,
+      },
     },
+  },
 });
