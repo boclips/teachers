2a0515003e17311e4cb028c80b3c1874cafe1cee
Benjamin Gröhbiel <ben@boclips.com>
2018-09-14 10:28:52 +0100

    Add pointer for thumbnail
    [#159849705]

--- src/boclips-player/BoclipsPlayer.test.tsx
+++ src/boclips-player/BoclipsPlayer.test.tsx
@@ -1,4 +1,4 @@
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import { By } from '../test-support/By';
 import BoclipsPlayer from './BoclipsPlayer';
