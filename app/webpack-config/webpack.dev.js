37fdbd34295f9798556ff4ea273826b94307e44c
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-09-21 11:44:02 +0200

    Use staging tracker by default
    We need to think about how to parametrise apps. This is a big discussion
    that I would like to postpone. Until then we use one tracker for
    staging/production environment.
    
    I have a hunch, without url changes GA does not provide us with
    conclusive information.
    
    [#160482128]

--- app/webpack-config/webpack.dev.js
+++ app/webpack-config/webpack.dev.js
@@ -1,31 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-const googleAnalyticsId = 'UA-126218810-1';
-const stagingUrl = 'https://video-service.staging-boclips.com/';
-const localUrl = 'http://localhost:8080/';
-const srcPath = path.resolve(__dirname, '../src');
+const googleAnalyticsId = "does-not-matter";
+const stagingUrl = "https://video-service.staging-boclips.com/";
+const srcPath = path.resolve(__dirname, "../src");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'eval-source-map',
+  mode: "development",
+  devtool: "eval-source-map",
   devServer: {
     historyApiFallback: true,
     proxy: {
-      '/v1/**': {
+      "/v1/**": {
         target: stagingUrl,
-        changeOrigin: true,
-      },
-    },
+        changeOrigin: true
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(srcPath, 'index.html'),
+      template: path.resolve(srcPath, "index.html"),
       ga: googleAnalyticsId
-    }),
+    })
   ]
 });
