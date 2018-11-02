f53aa50be726f4589a623c58abe26780134b1809
Thea Kramer <thea@boclips.com>
2018-11-02 16:41:53 +0000

    Add markup for error page
    [#161508167]

---
+++ src/error/Error.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import noResultsIllustration from '../images/no-results-illustration.png';
+
+
+export default class Error extends React.Component {
+
+  public render() {
+    return (
+      <section className="ant-layout-content error">
+        <div className="ant-col-12">
+          <img className="ant-col-20" src={noResultsIllustration} />
+        </div>
+        <div className="ant-col-12 error-message">
+            <div>
+              <h1>
+                Oops!!
+              </h1>
+              <p>
+                Friendly error description goes here.<br/><br/>
+                You can start a new search in the top bar or <br/>
+                <a href="/">return to the homepage</a>
+              </p>
+            </div>
+        </div>
+      </section>
+    );
+  }
+}
