7f48af63c56fd75507299950c3c8ba473de03e2d
Benjamin Gröhbiel <ben@boclips.com>
2018-11-06 11:36:38 +0000

    Update video playback data shape
    [#161285769]
    
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/videos/convertVideoResource.ts
+++ src/videos/convertVideoResource.ts
@@ -6,10 +6,10 @@ export default function convertVideoResource(resource: any): Video {
     id: resource.id,
     title: resource.title,
     description: resource.description,
-    duration: moment.duration(resource.duration),
+    duration: moment.duration(resource.playback.duration),
     releasedOn: new Date(resource.releasedOn),
     contentProvider: resource.contentProvider,
-    thumbnailUrl: resource.thumbnailUrl,
-    streamUrl: resource.streamUrl,
+    thumbnailUrl: resource.playback.thumbnailUrl,
+    streamUrl: resource.playback.streamUrl,
   };
 }
