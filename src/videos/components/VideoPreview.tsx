20bb227403a330749fabcd1c42781cab5684eae2
Thea Kramer <thea@boclips.com>
2018-11-09 14:05:54 +0000

    Make linter happy
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- src/videos/components/VideoPreview.tsx
+++ src/videos/components/VideoPreview.tsx
@@ -50,7 +50,11 @@ export default class VideoPreview extends React.PureComponent<Props> {
             text={`${AppConfig.getHost()}/videos/${this.props.video.id}`}
             onCopy={this.showCopiedNotification}
           >
-            <Button data-qa="copy-link" size={'large'} className={'secondary copy-link-button'}>
+            <Button
+              data-qa="copy-link"
+              size={'large'}
+              className={'secondary copy-link-button'}
+            >
               Copy link
             </Button>
           </CopyToClipboard>
