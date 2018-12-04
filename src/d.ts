99a8527b60238967f9973f71deb0106e1d1bed5c
José Carlos Valero Sánchez <josecarlos@boclips.com>
2018-12-04 14:14:53 +0000

    Fix ANTD regression: https://github.com/ant-design/ant-design/issues/13405
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- src/d.ts
+++ src/d.ts
@@ -2,3 +2,9 @@ declare module '*.png' {
   // @ts-ignore
   export default '' as string;
 }
+
+interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
