20234aefc38565466f8496a4b8f6da09ac0be50d
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-12-08 17:41:13 +0000

    Rewrite links for running the app locally
    App is pointing to staging.
    
    [#162484754]

--- webpack-config/webpack.dev.js
+++ webpack-config/webpack.dev.js
@@ -8,19 +8,39 @@ const common = require("./webpack.common.js");
 const googleAnalyticsId = "does-not-matter";
 const stagingUrl = "https://api.testing-boclips.com";
 const srcPath = path.resolve(__dirname, "../src");
+const localPort = 8081;
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
     historyApiFallback: true,
-    port: 8081,
+    port: localPort,
     proxy: {
       "/v1/**": {
         target: stagingUrl,
-        changeOrigin: true
+        changeOrigin: true,
+        onProxyRes: function (proxyRes, req, res) {
+          var originalBody = Buffer.from([]);
+
+          proxyRes.on('data', function (data) {
+            originalBody = Buffer.concat([originalBody, data]);
+          });
+
+          proxyRes.on('end', function () {
+            var apiUrlPattern = 'https:\\/\\/api\\.testing-boclips\\.com\\/v1\\/';
+            var proxiedApiUrl = `http://localhost:${localPort}/v1/`;
+            const bodyWithRewrittenLinks =
+              originalBody.toString('UTF-8').replace(new RegExp(apiUrlPattern, 'gm'), proxiedApiUrl);
+            console.log(`Proxied request... replacing ${apiUrlPattern} with ${proxiedApiUrl}`);
+            res.status(proxyRes.statusCode);
+            res.write(bodyWithRewrittenLinks);
+            res.end();
+          });
+        },
+        selfHandleResponse: true,
       }
-    }
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
