ab5266c2b1444c200f4cb164a8278518125a655e
Jacek Rzeniewicz <jacek@boclips.com>
2018-12-19 13:17:34 +0100

    Add subjects to video-service-responses
    [#162410795]

--- src/video-service-responses.ts
+++ src/video-service-responses.ts
@@ -15,34 +15,37 @@ export const links = {
   },
 };
 
+export const video177 = Object.freeze({
+  id: '177',
+  title: 'KS3/4 Science: Demonstrating Chemistry',
+  description: 'Matthew Tosh shows us the science.',
+  releasedOn: '2018-02-11',
+  contentProvider: 'cp1',
+  subjects: ['Maths', 'Physics'],
+  playback: {
+    thumbnailUrl: 'https://cdn.kaltura.com/thumbs/177.jpg',
+    streamUrl: 'https://cdn.kaltura.com/stream/147.mpd',
+    duration: 'PT1M2S',
+  },
+});
+
+const video147 = Object.freeze({
+  id: '147',
+  title: 'KS3/4 Science: Big Screen Science',
+  description: 'The winners of the North West round.',
+  releasedOn: '2018-02-12',
+  contentProvider: 'cp2',
+  subjects: [],
+  playback: {
+    thumbnailUrl: 'https://cdn.kaltura.com/thumbs/147.jpg',
+    streamUrl: 'https://cdn.kaltura.com/stream/147.mpd',
+    duration: 'PT1M3S',
+  },
+});
+
 export const videos = {
   _embedded: {
-    videos: [
-      {
-        id: '177',
-        title: 'KS3/4 Science: Demonstrating Chemistry',
-        description: 'Matthew Tosh shows us the science.',
-        releasedOn: '2018-02-11',
-        contentProvider: 'cp1',
-        playback: {
-          thumbnailUrl: 'https://cdn.kaltura.com/thumbs/177.jpg',
-          streamUrl: 'https://cdn.kaltura.com/stream/147.mpd',
-          duration: 'PT1M2S',
-        },
-      },
-      {
-        id: '147',
-        title: 'KS3/4 Science: Big Screen Science',
-        description: 'The winners of the North West round.',
-        releasedOn: '2018-02-12',
-        contentProvider: 'cp2',
-        playback: {
-          thumbnailUrl: 'https://cdn.kaltura.com/thumbs/147.jpg',
-          streamUrl: 'https://cdn.kaltura.com/stream/147.mpd',
-          duration: 'PT1M3S',
-        },
-      },
-    ],
+    videos: [video177, video147],
   },
   page: {
     size: 10,
@@ -52,25 +55,13 @@ export const videos = {
   },
 };
 
-export const video177 = {
-  id: '177',
-  title: 'KS3/4 Science: Demonstrating Chemistry',
-  description: 'Matthew Tosh shows us the science.',
-  releasedOn: '2018-02-11',
-  contentProvider: 'cp1',
-  playback: {
-    thumbnailUrl: 'https://cdn.kaltura.com/thumbs/177.jpg',
-    streamUrl: 'https://cdn.kaltura.com/stream/147.mpd',
-    duration: 'PT1M2S',
-  },
-};
-
 export const youtubeVideo1 = {
   id: '1',
   title: 'A youtube video',
   description: 'Matthew Tosh shows us the science.',
   releasedOn: '2018-02-11',
   contentProvider: 'cp1',
+  subjects: [],
   playback: {
     thumbnailUrl: 'https://cdn.kaltura.com/thumbs/177.jpg',
     youtubeId: 'youtubeId',
