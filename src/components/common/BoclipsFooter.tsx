b1ed4e5abbca5659d6581145500b2b90fe88438c
Matt Jones <matt@boclips.com>
2019-01-17 15:02:43 +0000

    Update copyright year automatically
    [#163189366]
    
    Signed-off-by: Oli Peate <oli@boclips.com>

--- src/components/common/BoclipsFooter.tsx
+++ src/components/common/BoclipsFooter.tsx
@@ -3,10 +3,14 @@ import React, { PureComponent } from 'react';
 
 const { Footer } = Layout;
 export class BoclipsFooter extends PureComponent {
+  private getCurrentYear() {
+    return new Date().getFullYear();
+  }
+
   public render() {
     return (
       <Footer className="boclips-footer">
-        <p>Copyright © 2018 Boclips. All rights reserved.</p>
+        <p>Copyright © {this.getCurrentYear()} Boclips. All rights reserved.</p>
         <p>
           All trademarks, service marks, trade names, product names and logos
           appearing on the site are the property of their respective owners. Any
