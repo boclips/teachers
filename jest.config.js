72a0e683889d5e11791618533fd5d1c9137ca847
Oli Peate <oli@boclips.com>
2019-02-11 15:35:29 +0000

    Use Jest's built-in JS-DOM mode

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,34 @@
 module.exports = {
-
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
-      'tsConfigFile': 'tsconfig.json'
-    }
+      tsConfigFile: 'tsconfig.json',
+    },
   },
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js'
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
 
   moduleNameMapper: {
     'boclips-js-security': '<rootDir>/__mocks__/boclipsJsSecurityMock.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
 
   // The test environment that will be used for testing
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    '**/*.(integrationTest|test).(ts|tsx|js)'
-  ],
+  testMatch: ['**/*.(integrationTest|test).(ts|tsx|js)'],
 
-  testPathIgnorePatterns: [
-    'node_modules',
-    'boclips-react-player',
-  ],
+  testPathIgnorePatterns: ['node_modules', 'boclips-react-player'],
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 
-  setupTestFrameworkScriptFile: './testSetup.tsx'
-
+  setupTestFrameworkScriptFile: './testSetup.tsx',
 };
--- testSetup.tsx
+++ testSetup.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
-import { JSDOM } from 'jsdom';
 import * as React from 'react';
 import MockFetchVerify from './test-support/MockFetchVerify';
 
@@ -12,42 +11,6 @@ Enzyme.configure({ adapter: new Adapter() });
 jest.mock('boclips-react-player', () => ({
   BoclipsPlayer: () => <div id="a-player" />,
 }));
-
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
-
-global['window'] = window;
-global['document'] = window.document;
-global['navigator'] = {
-  userAgent: 'node.js',
-};
-
-const location = JSON.stringify(global['window'].location);
-delete window.location;
-
-Object.defineProperty(window, 'location', {
-  value: JSON.parse(location),
-});
-
-Object.defineProperty(window.location, 'hostname', {
-  value: 'http://teachers.testing-boclips.com/',
-  configurable: true,
-});
-
-copyProps(window, global);
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce(
-      (result, prop) => ({
-        ...result,
-        [prop]: Object.getOwnPropertyDescriptor(src, prop),
-      }),
-      {},
-    );
-  Object.defineProperties(target, props);
-}
 
 beforeEach(() => {
   (axios.interceptors.request as any).handlers = [];
