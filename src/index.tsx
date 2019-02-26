fada34760b2ad85726e7a07aea921980e6632415
Oli Peate <oli@boclips.com>
2019-02-26 12:09:16 +0000

    Switch from Stackdriver to Sentry for error reporting
    The DSN is committed in the source because it's not a secret
    in a browser app - it gets shipped to the client.
    
    The origin/referrer for errors are locked down to
    https://*.staging-boclips.com and https://*.boclips.com

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
+import * as Sentry from '@sentry/browser';
 import React from 'react';
 import ReactDom from 'react-dom';
-import { StackdriverErrorReporter } from 'stackdriver-errors-js';
 import App from './app/App';
 import './index.less';
 
 const environment = process.env.NODE_ENV;
 
-if (environment !== 'development') {
-  window.addEventListener('DOMContentLoaded', () => {
-    // https://github.com/GoogleCloudPlatform/stackdriver-errors-js/issues/15#issuecomment-306594058
-    (window as any).StackTrace = require('stacktrace-js');
-    const errorHandler = new StackdriverErrorReporter();
-    errorHandler.start({
-      key: 'AIzaSyDccbot7jB0CeNid_GY3jaPKylhk1DBVLA',
-      projectId: 'boclips-prod',
-    });
+if (environment === 'production') {
+  Sentry.init({
+    dsn: 'https://0da84012df0643bd8876a2336cbe5d92@sentry.io/1402914',
   });
 }
 
