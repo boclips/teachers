2f31f65e1cca538c8edf97447d38ed7461ecda91
Matt Jones <matt@boclips.com>
2019-04-08 15:25:34 +0100

    Add logo to registration form

--- src/components/account/CreateAccountForm.tsx
+++ src/components/account/CreateAccountForm.tsx
@@ -131,210 +131,188 @@ class RegistrationForm extends React.Component<
     const { getFieldDecorator } = this.props.form;
     return (
       <section className="create-account-form__container">
-        <Row>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 14, push: 5 }}
-            xl={{ span: 12, push: 12 }}
+        <Form onSubmit={this.handleSubmit}>
+          <h1 className="alt create-account-form__title">Create account</h1>
+
+          <section className="create-account-form__form">
+            <TwoColumnInlineForm
+              leftColumn={getFieldDecorator('firstName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please enter your first name',
+                  },
+                ],
+              })(
+                <Input
+                  data-qa="first-name"
+                  size="large"
+                  placeholder="First name"
+                  className="create-account-form__first-name"
+                />,
+              )}
+              rightColumn={getFieldDecorator('lastName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please enter your last name',
+                  },
+                ],
+              })(
+                <Input
+                  data-qa="last-name"
+                  size="large"
+                  placeholder="Last name"
+                  className="create-account-form__last-name"
+                />,
+              )}
+            />
+
+            <Form.Item>
+              {getFieldDecorator('subjects', {
+                rules: [],
+              })(
+                <Input
+                  data-qa="subjects"
+                  size="large"
+                  placeholder="Subject(s) taught"
+                />,
+              )}
+            </Form.Item>
+
+            <Row>
+              <Col xs={{ span: 24 }} md={{ span: 16 }} xl={{ span: 16 }}>
+                <Form.Item>
+                  {getFieldDecorator('email', {
+                    rules: [
+                      {
+                        type: 'email',
+                        message: 'The input is not valid email',
+                      },
+                      {
+                        required: true,
+                        message: 'Please enter your email',
+                      },
+                    ],
+                  })(
+                    <Input data-qa="email" size="large" placeholder="Email" />,
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <TwoColumnInlineForm
+              leftColumn={getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    min: 8,
+                    message: 'Please enter at least 8 characters',
+                  },
+                  {
+                    validator: this.validateToNextPassword,
+                  },
+                ],
+              })(
+                <Input
+                  data-qa="password"
+                  size="large"
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+              rightColumn={getFieldDecorator('confirm', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please confirm your password',
+                  },
+                  {
+                    validator: this.compareToFirstPassword,
+                  },
+                ],
+              })(
+                <Input
+                  data-qa="password-confirm"
+                  size="large"
+                  type="password"
+                  onBlur={this.handleConfirmBlur}
+                  placeholder="Confirm password"
+                />,
+              )}
+            />
+
+            <Form.Item>
+              {getFieldDecorator('privacy_policy', {
+                rules: [
+                  {
+                    required: true,
+                  },
+                ],
+              })(
+                <Checkbox className="create-account-form__checkbox">
+                  I have read and agree with the Boclips{' '}
+                  <Link className="create-account-form__checkbox-link" to={'#'}>
+                    Terms and Conditions
+                  </Link>
+                  . Boclips will collect and process data as described in the{' '}
+                  <Link className="create-account-form__checkbox-link" to={'#'}>
+                    Privacy Policy
+                  </Link>
+                  .
+                </Checkbox>,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('hasOptedIntoMarketing', {
+                rules: [],
+              })(
+                <Checkbox className="create-account-form__checkbox">
+                  I want to receive marketing information about Boclips's
+                  similar products or services which may be of interest to me in
+                  accordance with the{' '}
+                  <Link className="create-account-form__checkbox-link" to={'#'}>
+                    Privacy Policy
+                  </Link>
+                  .
+                </Checkbox>,
+              )}
+            </Form.Item>
+          </section>
+
+          <div style={{ display: 'none' }}>
+            <Form.Item>
+              {getFieldDecorator('referralCode', {
+                rules: [],
+                initialValue: this.props.referralCode,
+              })(<Input type="text" />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('analyticsId', {
+                rules: [],
+                initialValue: AnalyticsFactory.getInstance().getId(),
+              })(<Input type="text" />)}
+            </Form.Item>
+          </div>
+
+          <Button
+            data-qa="register-button"
+            className="create-account-form__submit"
+            size="large"
+            type="primary"
+            htmlType="submit"
+            disabled={
+              this.state.creating ||
+              !this.props.form.getFieldValue('privacy_policy')
+            }
+            loading={this.state.creating}
           >
-            <Form onSubmit={this.handleSubmit}>
-              <h1 className="alt create-account-form__title">Create account</h1>
-
-              <section className="create-account-form__form">
-                <TwoColumnInlineForm
-                  leftColumn={getFieldDecorator('firstName', {
-                    rules: [
-                      {
-                        required: true,
-                        message: 'Please enter your first name',
-                      },
-                    ],
-                  })(
-                    <Input
-                      data-qa="first-name"
-                      size="large"
-                      placeholder="First name"
-                      className="create-account-form__first-name"
-                    />,
-                  )}
-                  rightColumn={getFieldDecorator('lastName', {
-                    rules: [
-                      {
-                        required: true,
-                        message: 'Please enter your last name',
-                      },
-                    ],
-                  })(
-                    <Input
-                      data-qa="last-name"
-                      size="large"
-                      placeholder="Last name"
-                      className="create-account-form__last-name"
-                    />,
-                  )}
-                />
-
-                <Form.Item>
-                  {getFieldDecorator('subjects', {
-                    rules: [],
-                  })(
-                    <Input
-                      data-qa="subjects"
-                      size="large"
-                      placeholder="Subject(s) taught"
-                    />,
-                  )}
-                </Form.Item>
-
-                <Row>
-                  <Col xs={{ span: 24 }} md={{ span: 16 }} xl={{ span: 16 }}>
-                    <Form.Item>
-                      {getFieldDecorator('email', {
-                        rules: [
-                          {
-                            type: 'email',
-                            message: 'The input is not valid email',
-                          },
-                          {
-                            required: true,
-                            message: 'Please enter your email',
-                          },
-                        ],
-                      })(
-                        <Input
-                          data-qa="email"
-                          size="large"
-                          placeholder="Email"
-                        />,
-                      )}
-                    </Form.Item>
-                  </Col>
-                </Row>
-
-                <TwoColumnInlineForm
-                  leftColumn={getFieldDecorator('password', {
-                    rules: [
-                      {
-                        required: true,
-                        min: 8,
-                        message: 'Please enter at least 8 characters',
-                      },
-                      {
-                        validator: this.validateToNextPassword,
-                      },
-                    ],
-                  })(
-                    <Input
-                      data-qa="password"
-                      size="large"
-                      type="password"
-                      placeholder="Password"
-                    />,
-                  )}
-                  rightColumn={getFieldDecorator('confirm', {
-                    rules: [
-                      {
-                        required: true,
-                        message: 'Please confirm your password',
-                      },
-                      {
-                        validator: this.compareToFirstPassword,
-                      },
-                    ],
-                  })(
-                    <Input
-                      data-qa="password-confirm"
-                      size="large"
-                      type="password"
-                      onBlur={this.handleConfirmBlur}
-                      placeholder="Confirm password"
-                    />,
-                  )}
-                />
-
-                <Form.Item>
-                  {getFieldDecorator('privacy_policy', {
-                    rules: [
-                      {
-                        required: true,
-                      },
-                    ],
-                  })(
-                    <Checkbox className="create-account-form__checkbox">
-                      I have read and agree with the Boclips{' '}
-                      <Link
-                        className="create-account-form__checkbox-link"
-                        to={'#'}
-                      >
-                        Terms and Conditions
-                      </Link>
-                      . Boclips will collect and process data as described in
-                      the{' '}
-                      <Link
-                        className="create-account-form__checkbox-link"
-                        to={'#'}
-                      >
-                        Privacy Policy
-                      </Link>
-                      .
-                    </Checkbox>,
-                  )}
-                </Form.Item>
-                <Form.Item>
-                  {getFieldDecorator('hasOptedIntoMarketing', {
-                    rules: [],
-                  })(
-                    <Checkbox className="create-account-form__checkbox">
-                      I want to receive marketing information about Boclips's
-                      similar products or services which may be of interest to
-                      me in accordance with the{' '}
-                      <Link
-                        className="create-account-form__checkbox-link"
-                        to={'#'}
-                      >
-                        Privacy Policy
-                      </Link>
-                      .
-                    </Checkbox>,
-                  )}
-                </Form.Item>
-              </section>
-
-              <div style={{ display: 'none' }}>
-                <Form.Item>
-                  {getFieldDecorator('referralCode', {
-                    rules: [],
-                    initialValue: this.props.referralCode,
-                  })(<Input type="text" />)}
-                </Form.Item>
-                <Form.Item>
-                  {getFieldDecorator('analyticsId', {
-                    rules: [],
-                    initialValue: AnalyticsFactory.getInstance().getId(),
-                  })(<Input type="text" />)}
-                </Form.Item>
-              </div>
-
-              <Button
-                data-qa="register-button"
-                className="create-account-form__submit"
-                size="large"
-                type="primary"
-                htmlType="submit"
-                disabled={
-                  this.state.creating ||
-                  !this.props.form.getFieldValue('privacy_policy')
-                }
-                loading={this.state.creating}
-              >
-                Register
-              </Button>
-            </Form>
-            <p className="create-account-form__existing">
-              Already have an account? <Link to={'/'}>Login</Link>
-            </p>
-          </Col>
-        </Row>
+            Register
+          </Button>
+        </Form>
+        <p className="create-account-form__existing">
+          Already have an account? <Link to={'/'}>Login</Link>
+        </p>
       </section>
     );
   }
--- src/views/account/CreateAccountView.tsx
+++ src/views/account/CreateAccountView.tsx
@@ -1,4 +1,6 @@
+import { Col, Row } from 'antd';
 import React, { PureComponent } from 'react';
+import registrationLogo from '../../../resources/images/registration-logo.svg';
 import CreateAccountForm from '../../components/account/CreateAccountForm';
 import PageLayout from '../../components/layout/PageLayout';
 import './CreateAccountView.less';
@@ -8,7 +10,16 @@ export class CreateAccountView extends PureComponent {
     return (
       <PageLayout>
         <section className="create-account" data-qa="create-account-page">
-          {this.renderForm()}
+          <Row>
+            <Col xs={{ span: 0 }} lg={{ span: 12 }}>
+              <img className="create-account__logo" src={registrationLogo} />
+            </Col>
+            <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+              <div className="create-account__form">
+                <CreateAccountForm />
+              </div>
+            </Col>
+          </Row>
         </section>
       </PageLayout>
     );
