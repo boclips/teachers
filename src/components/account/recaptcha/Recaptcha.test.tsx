5a6bf4e4592c8124ce5951155fe1d9b921869c3d
Jacek Rzeniewicz <jacek@rzrz.pl>
2019-04-09 13:49:27 +0200

    Revert "Revert "Include a Google reCaptcha token on New User form""
    This reverts commit 540a3e2e93e50782ab8f5847ad0cbe5dad2a7dfd.

---
+++ src/components/account/recaptcha/Recaptcha.test.tsx
@@ -0,0 +1,25 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+import { ReCaptcha } from 'react-recaptcha-v3';
+import { Props, Recaptcha } from './Recaptcha';
+
+describe('Recaptcha', () => {
+  const getComponent = (props: Props) => {
+    return mount(<Recaptcha {...props} />);
+  };
+
+  it('Configures the Recaptcha component', () => {
+    const verifyCallback = token => console.log(token);
+    const component = getComponent({
+      verifyCallback,
+    });
+
+    const childRecaptcha = component.find(ReCaptcha);
+
+    expect(childRecaptcha.prop('sitekey')).toBe(
+      '6Ldi6JwUAAAAACMgLoshiwBEZNDmnllXrAEIMN_y',
+    );
+    expect(childRecaptcha.prop('action')).toBe('registration');
+    expect(childRecaptcha.prop('verifyCallback')).toBe(verifyCallback);
+  });
+});
