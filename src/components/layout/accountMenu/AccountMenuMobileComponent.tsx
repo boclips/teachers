4c8e7a9093facef103ab790136067aab0c6e9430
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2019-04-11 07:41:12 +0100

    Add Bookmarks to mobile menu
    [#164780073]

--- src/components/layout/accountMenu/AccountMenuMobileComponent.tsx
+++ src/components/layout/accountMenu/AccountMenuMobileComponent.tsx
@@ -4,7 +4,11 @@ import collectionsIcon from '../../../../resources/images/collections-grey.svg';
 import logooutIcon from '../../../../resources/images/logout.svg';
 import AccountMenuIconComponent from './AccountMenuIconComponent';
 import './AccountMenuMobile.less';
-import { LogoutLink, VideoCollectionsLink } from './MenuOptions';
+import {
+  BookmarkedCollectionsLink,
+  LogoutLink,
+  VideoCollectionsLink,
+} from './MenuOptions';
 
 interface State {
   visible: boolean;
@@ -46,7 +50,7 @@ export default class AccountMenuMobile extends React.PureComponent<
           placement="bottom"
           title="My account"
           onClose={this.onClose}
-          height="142"
+          height="185"
         >
           <Menu selectable={false}>
             <Menu.Item key="1" className="account-menu-mobile__menu-item">
@@ -60,8 +64,20 @@ export default class AccountMenuMobile extends React.PureComponent<
                 onClick={this.onClose}
               />
             </Menu.Item>
-            <Menu.Divider key="lovely-divider" />
+            <Menu.Divider key="lovely-divider-1" />
             <Menu.Item key="2" className="account-menu-mobile__menu-item">
+              <BookmarkedCollectionsLink
+                logo={
+                  <img
+                    src={collectionsIcon}
+                    className="account-menu-mobile__image"
+                  />
+                }
+                onClick={this.onClose}
+              />
+            </Menu.Item>
+            <Menu.Divider key="lovely-divider-2" />
+            <Menu.Item key="3" className="account-menu-mobile__menu-item">
               <LogoutLink
                 logo={
                   <img
