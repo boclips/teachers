913a673a82006bf6687fdfaca60b8e942c26c1bb
Matt Jones <matt@boclips.com>
2019-04-23 10:20:36 +0100

    Refactor store collections to seperate file
    [#165441993]

--- src/components/collection/redux/reducers/collectionsReducer.ts
+++ src/components/collection/redux/reducers/collectionsReducer.ts
@@ -26,13 +26,15 @@ import { onCollectionUnbookmarkedAction } from '../actions/onCollectionUnbookmar
 import { removeFromCollectionAction } from '../actions/removeFromCollectionAction';
 import { removeFromCollectionResultAction } from '../actions/removeFromCollectionResultAction';
 import { storeCollectionAction } from '../actions/storeCollectionAction';
-import {
-  storeCollectionsAction,
-  StoreCollectionsRequest,
-} from '../actions/storeCollectionsAction';
+import { storeCollectionsAction } from '../actions/storeCollectionsAction';
 import { storeVideoForCollectionAction } from '../actions/storeVideoForCollectionAction';
 import { UpdateCollectionResult } from '../middleware/addToCollectionResultMiddleware';
 import { VideoMap } from './../../../../types/VideoCollection';
+import {
+  onStoreCollectionAction,
+  onStoreCollectionsAction,
+  onStoreVideosForCollectionAction,
+} from './storeCollectionsReducer';
 
 const initialState: CollectionsStateValue = {
   myCollections: [],
@@ -294,168 +296,6 @@ const onCollectionEdited = (
     myCollections,
   };
 };
-
-const onStoreCollectionsAction = (
-  state: CollectionsStateValue,
-  request: StoreCollectionsRequest,
-): CollectionsStateValue => {
-  return {
-    ...state,
-    [request.key]: request.collections,
-    loading: false,
-    updating: false,
-  };
-};
-
-const onStoreCollectionAction = (
-  state: CollectionsStateValue,
-  collectionDetails: VideoCollection,
-): CollectionsStateValue => {
-  return {
-    ...state,
-    publicCollectionDetails: collectionDetails,
-    loading: false,
-    updating: false,
-  };
-};
-
-const onStoreVideosForCollectionAction = (
-  state: CollectionsStateValue,
-  request: { videos: Video[]; collection: VideoCollection },
-): CollectionsStateValue => {
-  state = reduceStoreVideoForMyCollections(state, request);
-  state = reduceStoreVideoForPageableCollections(
-    state,
-    'bookmarkedCollections',
-    request,
-  );
-  state = reduceStoreVideoForPageableCollections(
-    state,
-    'publicCollections',
-    request,
-  );
-  return reduceStoreVideoForCollectionDetails(state, request);
-};
-
-const reduceStoreVideoForMyCollections = (
-  state: CollectionsStateValue,
-  request: { videos: Video[]; collection: VideoCollection },
-): CollectionsStateValue => {
-  const myCollections = updateMatchingCollectionWithVideos(
-    request,
-    state.myCollections,
-  );
-
-  return { ...state, myCollections };
-};
-
-const reduceStoreVideoForPageableCollections = (
-  state: CollectionsStateValue,
-  key: 'bookmarkedCollections' | 'publicCollections',
-  request: {
-    videos: Video[];
-    collection: VideoCollection;
-  },
-): CollectionsStateValue => {
-  if (!state[key] || !state[key].items) {
-    return state;
-  }
-
-  const collectionItems = updateMatchingCollectionWithVideos(
-    request,
-    state[key].items,
-  );
-
-  return {
-    ...state,
-    [key]: {
-      ...state[key],
-      items: collectionItems,
-    },
-  };
-};
-
-const reduceStoreVideoForCollectionDetails = (
-  state: CollectionsStateValue,
-  request: { videos: Video[]; collection: VideoCollection },
-): CollectionsStateValue => {
-  if (
-    !state.publicCollectionDetails ||
-    state.publicCollectionDetails.id !== request.collection.id
-  ) {
-    return state;
-  }
-
-  const collectionDetails = addVideosToCollection(request);
-
-  return {
-    ...state,
-    publicCollectionDetails: collectionDetails,
-  };
-};
-
-const updateMatchingCollectionWithVideos = (
-  request: {
-    videos: Video[];
-    collection: VideoCollection;
-  },
-  collections: Readonly<VideoCollection[]>,
-): VideoCollection[] => {
-  const collectionItems = [...collections];
-
-  const indexOfCollectionToUpdate = getIndexOfCollection(
-    collectionItems,
-    request.collection.id,
-  );
-
-  if (indexOfCollectionToUpdate < 0) {
-    return collectionItems;
-  }
-
-  const updatedCollection = addVideosToCollection(request);
-
-  collectionItems[indexOfCollectionToUpdate] = updatedCollection;
-
-  return collectionItems;
-};
-
-const addVideosToCollection = (request: {
-  videos: Video[];
-  collection: VideoCollection;
-}): VideoCollection => {
-  const videos = request.collection.videos;
-
-  if (videosAlreadyLoaded(request, videos)) {
-    return request.collection;
-  }
-
-  const videoMapToUpdate: VideoMap = getVideoMapToUpdate(request);
-
-  return {
-    ...request.collection,
-    videos: {
-      ...videos,
-      ...videoMapToUpdate,
-    },
-  };
-};
-
-function getVideoMapToUpdate(request: {
-  videos: Video[];
-  collection: VideoCollection;
-}) {
-  return request.videos.reduce((map, video) => {
-    map[video.id] = video;
-    return map;
-  }, {});
-}
-
-function videosAlreadyLoaded(
-  request: { videos: Video[]; collection: VideoCollection },
-  videos,
-) {
-  return request.videos.map(v => v.id).every(id => videos[id] !== undefined);
-}
 
 export const collectionsReducer: Reducer<CollectionsStateValue> = createReducer(
   initialState,
---
+++ src/components/collection/redux/reducers/storeCollectionsReducer.ts
@@ -0,0 +1,169 @@
+import {
+  CollectionsStateValue,
+  getIndexOfCollection,
+} from '../../../../types/State';
+import { Video } from '../../../../types/Video';
+import { VideoCollection, VideoMap } from '../../../../types/VideoCollection';
+import { StoreCollectionsRequest } from '../actions/storeCollectionsAction';
+
+export const onStoreCollectionsAction = (
+  state: CollectionsStateValue,
+  request: StoreCollectionsRequest,
+): CollectionsStateValue => {
+  return {
+    ...state,
+    [request.key]: request.collections,
+    loading: false,
+    updating: false,
+  };
+};
+
+export const onStoreCollectionAction = (
+  state: CollectionsStateValue,
+  collectionDetails: VideoCollection,
+): CollectionsStateValue => {
+  return {
+    ...state,
+    publicCollectionDetails: collectionDetails,
+    loading: false,
+    updating: false,
+  };
+};
+
+export const onStoreVideosForCollectionAction = (
+  state: CollectionsStateValue,
+  request: { videos: Video[]; collection: VideoCollection },
+): CollectionsStateValue => {
+  state = reduceStoreVideoForMyCollections(state, request);
+  state = reduceStoreVideoForPageableCollections(
+    state,
+    'bookmarkedCollections',
+    request,
+  );
+  state = reduceStoreVideoForPageableCollections(
+    state,
+    'publicCollections',
+    request,
+  );
+  return reduceStoreVideoForCollectionDetails(state, request);
+};
+
+const reduceStoreVideoForMyCollections = (
+  state: CollectionsStateValue,
+  request: { videos: Video[]; collection: VideoCollection },
+): CollectionsStateValue => {
+  const myCollections = updateMatchingCollectionWithVideos(
+    request,
+    state.myCollections,
+  );
+
+  return { ...state, myCollections };
+};
+
+const reduceStoreVideoForPageableCollections = (
+  state: CollectionsStateValue,
+  key: 'bookmarkedCollections' | 'publicCollections',
+  request: {
+    videos: Video[];
+    collection: VideoCollection;
+  },
+): CollectionsStateValue => {
+  if (!state[key] || !state[key].items) {
+    return state;
+  }
+
+  const collectionItems = updateMatchingCollectionWithVideos(
+    request,
+    state[key].items,
+  );
+
+  return {
+    ...state,
+    [key]: {
+      ...state[key],
+      items: collectionItems,
+    },
+  };
+};
+
+const reduceStoreVideoForCollectionDetails = (
+  state: CollectionsStateValue,
+  request: { videos: Video[]; collection: VideoCollection },
+): CollectionsStateValue => {
+  if (
+    !state.publicCollectionDetails ||
+    state.publicCollectionDetails.id !== request.collection.id
+  ) {
+    return state;
+  }
+
+  const collectionDetails = addVideosToCollection(request);
+
+  return {
+    ...state,
+    publicCollectionDetails: collectionDetails,
+  };
+};
+
+const updateMatchingCollectionWithVideos = (
+  request: {
+    videos: Video[];
+    collection: VideoCollection;
+  },
+  collections: Readonly<VideoCollection[]>,
+): VideoCollection[] => {
+  const collectionItems = [...collections];
+
+  const indexOfCollectionToUpdate = getIndexOfCollection(
+    collectionItems,
+    request.collection.id,
+  );
+
+  if (indexOfCollectionToUpdate < 0) {
+    return collectionItems;
+  }
+
+  const updatedCollection = addVideosToCollection(request);
+
+  collectionItems[indexOfCollectionToUpdate] = updatedCollection;
+
+  return collectionItems;
+};
+
+const addVideosToCollection = (request: {
+  videos: Video[];
+  collection: VideoCollection;
+}): VideoCollection => {
+  const videos = request.collection.videos;
+
+  if (videosAlreadyLoaded(request, videos)) {
+    return request.collection;
+  }
+
+  const videoMapToUpdate: VideoMap = getVideoMapToUpdate(request);
+
+  return {
+    ...request.collection,
+    videos: {
+      ...videos,
+      ...videoMapToUpdate,
+    },
+  };
+};
+
+function getVideoMapToUpdate(request: {
+  videos: Video[];
+  collection: VideoCollection;
+}) {
+  return request.videos.reduce((map, video) => {
+    map[video.id] = video;
+    return map;
+  }, {});
+}
+
+function videosAlreadyLoaded(
+  request: { videos: Video[]; collection: VideoCollection },
+  videos,
+) {
+  return request.videos.map(v => v.id).every(id => videos[id] !== undefined);
+}
