6af7f5244f3701f62480383b1bea6ca8dcb1ce95
Matt Jones <matt@boclips.com>
2019-04-30 17:55:37 +0100

    Only load hubspot script when in prod environment
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import './index.less';
+// CSS breaks when index.less joins the rest of the imports
 
 import * as Sentry from '@sentry/browser';
 import React from 'react';
@@ -7,10 +8,23 @@ import App from './app/App';
 
 const environment = process.env.NODE_ENV;
 
+const addHubspotScript = () => {
+  const hubspotScript = document.createElement('script');
+  hubspotScript.setAttribute('type', 'text/javascript');
+  hubspotScript.setAttribute('id', 'hs-script-loader');
+  hubspotScript.setAttribute('async', 'true');
+  hubspotScript.setAttribute('defer', 'true');
+  hubspotScript.src = '//js.hs-scripts.com/4854096.js';
+
+  document.head.appendChild(hubspotScript);
+};
+
 if (environment === 'production') {
   Sentry.init({
     dsn: 'https://0da84012df0643bd8876a2336cbe5d92@sentry.io/1402914',
   });
+
+  addHubspotScript();
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
