d76236fdb6269e4250147427a62f13182d3f37c1
José Carlos Valero Sánchez <josecarlos@boclips.com>
2019-05-07 12:15:55 +0100

    Add mobile version of video card menu
    [#165422219]

--- src/components/video/buttons/VideoButtons.tsx
+++ src/components/video/buttons/VideoButtons.tsx
@@ -1,5 +1,7 @@
-import { Button } from 'antd';
+import { Button, Dropdown, Icon, Menu } from 'antd';
+import { CustomIconComponentProps } from 'antd/lib/icon';
 import React from 'react';
+import moreSvg from '../../../../resources/images/more.svg';
 import { Video } from '../../../types/Video';
 import { VideoCollection } from '../../../types/VideoCollection';
 import CopyLinkButton from './copyLink/CopyLinkButton';
@@ -11,20 +13,60 @@ interface OwnProps {
   video: Video;
   collection?: VideoCollection;
 }
+const svg = moreSvg as React.ComponentType<CustomIconComponentProps>;
 
 export default class VideoButtons extends React.PureComponent<OwnProps> {
   public render() {
     return (
       <section className="video-buttons__container">
-        <Button.Group>
-          <VideoCollectionButton
-            video={this.props.video}
-            collection={this.props.collection}
-          />
-          <CopyLinkButton video={this.props.video} />
-          <DownloadTranscriptButton video={this.props.video} />
-        </Button.Group>
+        <section className="display-tablet-and-desktop">
+          <DesktopButtons {...this.props} />
+        </section>
+
+        <section className="display-mobile">
+          <MobileButtons {...this.props} />
+        </section>
       </section>
     );
   }
 }
+
+const DesktopButtons = (props: OwnProps) => {
+  return (
+    <Button.Group>
+      <VideoCollectionButton
+        video={props.video}
+        collection={props.collection}
+      />
+      <CopyLinkButton video={props.video} />
+      <DownloadTranscriptButton video={props.video} />
+    </Button.Group>
+  );
+};
+
+const MobileButtons = (props: OwnProps) => {
+  const menu = () => (
+    <Menu className="video-buttons__dropdown">
+      <Menu.Item key="1">
+        <CopyLinkButton video={props.video} />
+      </Menu.Item>
+      <Menu.Item key="2">
+        <DownloadTranscriptButton video={props.video} />
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Button.Group>
+      <VideoCollectionButton
+        video={props.video}
+        collection={props.collection}
+      />
+      <Dropdown overlay={menu()} trigger={['click']}>
+        <Button htmlType="button">
+          <Icon component={svg} />
+        </Button>
+      </Dropdown>
+    </Button.Group>
+  );
+};
