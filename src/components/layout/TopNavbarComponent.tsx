e5af3557cf0806ab9cd42ec66b7b29b1ff3209ac
José Carlos Valero Sánchez <josecarlos@boclips.com>
2019-05-15 11:26:48 +0100

    Box header
    [#165975710]

--- src/components/layout/TopNavbarComponent.tsx
+++ src/components/layout/TopNavbarComponent.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from 'antd';
+import Layout from 'antd/lib/layout';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import boclipsLogo from '../../../resources/images/boclips-logo.png';
@@ -11,24 +12,29 @@ interface Props {
   authorized: boolean;
   showSearchBar: boolean;
 }
+
+const { Content } = Layout;
+
 const TopNavbarComponent = React.memo((props: Props) => (
   <React.Fragment>
-    <Row>
-      <Col sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }}>
-        <section>
-          <Link to="/" data-qa="boclips-logo">
-            <img className="logo" src={boclipsLogo} alt="Boclips" />
-          </Link>
-        </section>
-      </Col>
-      <Col xs={{ span: 19 }} sm={{ span: 20 }} md={{ span: 14 }}>
-        {props.showSearchBar ? <SearchBar /> : null}
-      </Col>
-      <Col xs={{ span: 5 }} sm={{ span: 4 }} md={{ span: 4 }}>
-        {props.authorized && <AccountMenuContainer />}
-      </Col>
-    </Row>
-    {props.showTabs && <ConnectedTabsContainer data-qa="navbar-tabs" />}
+    <Content>
+      <Row>
+        <Col sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }}>
+          <section>
+            <Link to="/" data-qa="boclips-logo">
+              <img className="logo" src={boclipsLogo} alt="Boclips" />
+            </Link>
+          </section>
+        </Col>
+        <Col xs={{ span: 19 }} sm={{ span: 20 }} md={{ span: 14 }}>
+          {props.showSearchBar ? <SearchBar /> : null}
+        </Col>
+        <Col xs={{ span: 5 }} sm={{ span: 4 }} md={{ span: 4 }}>
+          {props.authorized && <AccountMenuContainer />}
+        </Col>
+      </Row>
+      {props.showTabs && <ConnectedTabsContainer data-qa="navbar-tabs" />}
+    </Content>
   </React.Fragment>
 ));
 
