1c21a9936a27bf5d9da02cabd1fffa99e5232ceb
Alex Glen <alex@boclips.com>
2019-06-12 12:20:20 +0100

    Finish google share button
    [#166125036]
    
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- src/components/video/buttons/gclassroom/GoogleClassroomShareButton.tsx
+++ src/components/video/buttons/gclassroom/GoogleClassroomShareButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Constants } from '../../../../app/AppConstants';
 import { Video } from '../../../../types/Video';
 import { gapi, ShareToClassroom } from './GoogleClassroomShareWrapper';
 
@@ -16,19 +17,17 @@ export class GoogleClassroomShareButton extends React.Component<Props> {
   public render() {
     return (
       <div>
-        <div
-          ref={this.ref}
-          data-size="32"
-          data-url="https://test.com/resource/1"
-          data-qa="gc-share-button"
-        />
+        <div ref={this.ref} data-qa="gc-share-button" />
       </div>
     );
   }
 
   public componentDidMount(): void {
     gapi.then((instance: ShareToClassroom) => {
-      instance.sharetoclassroom.render(this.ref.current);
+      instance.sharetoclassroom.render(this.ref.current, {
+        size: 32,
+        url: `${Constants.HOST}/videos/${this.props.video.id}`,
+      });
     });
   }
 }
