6006d2f861c7241efcfcf08459eb9a2f75d1d0e1
Antony O'Neill <antony@boclips.com>
2019-07-16 18:00:49 +0100

    Enable storybook in teachers!
    [#166955172]

---
+++ .storybook/config.js
@@ -0,0 +1,14 @@
+import { configure } from '@storybook/react';
+import '@storybook/addon-console';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport/src/defaults';
+
+const req = require.context('../src', true, /.stories\.tsx$/);
+
+function loadStories() {
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
+
+addParameters({ viewport: { viewports: INITIAL_VIEWPORTS } });
