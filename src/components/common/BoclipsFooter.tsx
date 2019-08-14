526a43048670fa3fc79d9e2453fba31fb1bbf66e
Alex Glen <alex@boclips.com>
2019-08-14 16:40:14 +0100

    Ruthlessly refactor the BoclipsFooter.
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/components/common/BoclipsFooter.tsx
+++ src/components/common/BoclipsFooter.tsx
@@ -1,21 +1,13 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import State from '../../types/State';
+import React from 'react';
 import './BoclipsFooter.less';
 
-interface StateProps {
-  isAuthenticated: boolean;
-}
-
-class BoclipsFooter extends PureComponent<StateProps> {
-  private getCurrentYear() {
-    return new Date().getFullYear();
-  }
-
+export default class BoclipsFooter extends React.PureComponent {
   public render() {
     return (
       <footer className="boclips-footer ant-layout-content">
-        <p>Copyright © {this.getCurrentYear()} Boclips. All rights reserved.</p>
+        <p>
+          Copyright © {new Date().getFullYear()} Boclips. All rights reserved.
+        </p>
         <p>
           All trademarks, service marks, trade names, product names and logos
           appearing on the site are the property of their respective owners. Any
@@ -25,16 +17,3 @@ class BoclipsFooter extends PureComponent<StateProps> {
     );
   }
 }
-
-function mapStateToProps(state: State): StateProps {
-  const user = state.user;
-  const isAuthenticated = user ? user.authenticated : false;
-  return {
-    isAuthenticated,
-  };
-}
-
-export default connect<StateProps, {}, {}>(
-  mapStateToProps,
-  null,
-)(BoclipsFooter);
