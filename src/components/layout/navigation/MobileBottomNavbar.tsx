4e32cbd6799b531f78630c84ae96985810ac4c80
Alex Glen <alex@boclips.com>
2019-11-11 16:25:31 +0000

    Don't show mobile navbar when not logged in
    -Should be authorized to see any navigation on video details page
    
    [#169676839]

--- src/components/layout/navigation/MobileBottomNavbar.tsx
+++ src/components/layout/navigation/MobileBottomNavbar.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { UserState } from '../../../types/State';
 import './MobileBottomNavbar.less';
 import NavbarButtonsContainer from './NavbarButtonsContainer';
 
-const MobileBottomNavbar = React.memo(() => (
-  <section className="mobile-bottom-navbar display-mobile-and-tablet">
-    <NavbarButtonsContainer />
-  </section>
-));
+interface StateProps {
+  authorized: boolean;
+}
 
-export default MobileBottomNavbar;
+class MobileBottomNavbar extends React.PureComponent<StateProps> {
+  public render() {
+    return (
+      this.props.authorized && (
+        <section className="mobile-bottom-navbar display-mobile-and-tablet">
+          <NavbarButtonsContainer />
+        </section>
+      )
+    );
+  }
+}
+
+function mapStateToProps(state: UserState): StateProps {
+  return {
+    authorized: !!state.user,
+  };
+}
+
+export default connect(mapStateToProps)(MobileBottomNavbar);
