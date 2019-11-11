6ca38f8af6d58b773cbaedc387c7f3ec0c2a4e91
Eva Varga <eva@boclips.com>
2019-11-11 17:35:32 +0000

    Add example unit test for CollectionCard using react-testing-library

--- src/components/collection/card/CollectionCard.test.tsx
+++ src/components/collection/card/CollectionCard.test.tsx
@@ -1,11 +1,17 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import {
+  CollectionsFactory,
+  EntitiesFactory,
+  RouterFactory,
+  SubjectFactory,
   VideoCollectionFactory,
   VideoCollectionLinksFactory,
   VideoFactory,
   VideoIdFactory,
 } from '../../../../test-support/factories';
+import { render } from '../../../../test-support/render';
+import { AgeRange } from '../../../types/AgeRange';
 import { Link } from '../../../types/Link';
 import { VideoCollection } from '../../../types/VideoCollection';
 import { noOp } from '../../../utils';
@@ -13,6 +19,7 @@ import { ClickableCard } from '../../common/ClickableCard/ClickableCard';
 import CollectionHeader from '../header/CollectionHeader';
 import { CollectionCard, Props } from './CollectionCard';
 import CollectionCardVideoPreviews from './CollectionCardVideoPreviews';
+import { within } from '@testing-library/react';
 
 const NUMBER_OF_PREVIEWS = 4;
 
@@ -68,4 +75,75 @@ describe('CollectionCard', () => {
     expect(clickableCard).toExist();
     expect(clickableCard.props().href).toEqual('/collections/1-2-3');
   });
+});
+
+describe('CollectionCard using testing-library', () => {
+  it('renders basic information and video previews when loaded', () => {
+    const firstVideo = VideoFactory.sample({ id: '1' });
+    const secondVideo = VideoFactory.sample({ id: '2' });
+    const subject = SubjectFactory.sample({ id: 'subject-id', name: 'Maths' });
+    const collection = VideoCollectionFactory.sample({
+      id: '1-2-3',
+      title: 'My best collection',
+      updatedAt: '2019-11-11T12:00:00.870Z',
+      videoIds: [firstVideo, secondVideo].map(it =>
+        VideoIdFactory.sample({ value: it.id }),
+      ),
+      subjects: [subject.id],
+      ageRange: new AgeRange(4, 12),
+      links: VideoCollectionLinksFactory.sample({
+        bookmark: new Link({ href: 'bookmark', templated: false }),
+        unbookmark: new Link({ href: 'unbookmark', templated: false }),
+      }),
+    });
+
+    const { getByText, getByTestId } = render(
+      <CollectionCard
+        collection={collection}
+        numberOfPreviews={4}
+        videos={[firstVideo, secondVideo]}
+      />,
+      {
+        initialState: {
+          router: RouterFactory.sample(),
+          entities: EntitiesFactory.sample({
+            collections: {
+              byId: {
+                [collection.id]: collection,
+              },
+            },
+          }),
+          collections: CollectionsFactory.sample(),
+          subjects: [subject],
+        },
+      },
+    );
+
+    const collectionCard = getByTestId('collection-card');
+
+    expect(getByText('My best collection')).toBeInTheDocument();
+    expect(getByText('Subject:')).toBeInTheDocument();
+    expect(getByText('Maths')).toBeInTheDocument();
+    expect(getByText('Ages:')).toBeInTheDocument();
+    expect(getByText('4-12')).toBeInTheDocument();
+    expect(within(collectionCard).getByRole('button')).toBeInTheDocument();
+  });
+
+  // 1. basic look
+  // renders title, subtitle
+  // renders actions
+  // renders video previews
+  // shows visibility (public or private)
+  // clicking on it redirects to collection page
+  // ---
+  // 2. edit click
+  // opens editor dialog
+  // changing title, submit => change reflected in card
+  // --
+  // 3. delete click
+  // opens warning dialog
+  // delete confirmation -> card is removed from dom
+  // --
+  // 4. can click on the video and start it from the card view
+  // --
 });
