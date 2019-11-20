a366d6df489a6e80c9169f0d1bfb2a1b0131014c
Alex Glen <alex@boclips.com>
2019-11-20 14:33:03 +0000

    Refactor search results card list
    [#169680491]
    
    Signed-off-by: Rebecca Appleyard <rebecca@boclips.com>

--- src/components/searchResults/SeachResultsCardList.tsx
+++ src/components/searchResults/SeachResultsCardList.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Video } from '../../types/Video';
 import { VideoCollection } from '../../types/VideoCollection';
-import { CardContainer } from './CardContainer';
+import CollectionCardContainer from '../collection/card/CollectionCardContainer';
+import { VideoCard } from '../video/card/VideoCard';
 import './SearchResultsCardList.less';
 
 interface Props {
@@ -16,17 +17,20 @@ export class SearchResultsCardList extends React.PureComponent<Props> {
   public render() {
     return (
       <TransitionGroup exit={true}>
-        {zip(this.props.videos, this.props.collections)
-          .flat()
-          .map((cardContent, index) => {
+        {zip(this.props.videos, this.props.collections).map(
+          ([video, collection], index) => {
             return (
               <CSSTransition key={index} classNames="card-list" timeout={500}>
                 <div>
-                  <CardContainer content={cardContent} searchIndex={index} />
+                  {video && <VideoCard video={video} videoIndex={index} />}
+                  {collection && (
+                    <CollectionCardContainer collection={collection} />
+                  )}
                 </div>
               </CSSTransition>
             );
-          })}
+          },
+        )}
       </TransitionGroup>
     );
   }
