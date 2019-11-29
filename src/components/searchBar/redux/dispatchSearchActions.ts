b8b58fb13363077302d7875144f5068fdc69f5cc
Boisababy Clips <bob@fleming.nowhere>
2019-11-29 13:32:19 +0000

    Revert "Don't search collections when filters applied"
    Turns out, the collectionSearch object persists after applying filters.
    This means that it still appears that the collections are being
    filtered.
    
    This reverts commit a0bd1a5c118c5066c6b23dd6938ef4615974f9d0.
    
    [#169680491]

--- src/components/searchBar/redux/dispatchSearchActions.ts
+++ src/components/searchBar/redux/dispatchSearchActions.ts
@@ -1,5 +1,3 @@
-import every from 'lodash/every';
-import isUndefined from 'lodash/isUndefined';
 import queryString from 'query-string';
 import { Store } from 'redux';
 import { Constants } from '../../../app/AppConstants';
@@ -8,20 +6,20 @@ import { RequestFilters } from '../../../types/VideoSearchRequest';
 import { searchCollectionsAction } from './actions/searchCollectionsActions';
 import { searchVideosAction } from './actions/searchVideosActions';
 
-const defaultFilters: Pick<RequestFilters, 'excludeTags' | 'includeTags'> = {
-  excludeTags: [Constants.NEWS],
-  includeTags: [Constants.CLASSROOM],
-};
+const getFilters = (queryParams: any): RequestFilters => {
+  const includeTags = [Constants.CLASSROOM];
+  const excludeTags = [Constants.NEWS];
 
-const getFilters = (
-  queryParams: any,
-): Omit<RequestFilters, 'excludeTags' | 'includeTags'> => ({
-  subject: queryParams.subject || undefined,
-  duration_min: +queryParams.duration_min || undefined,
-  duration_max: +queryParams.duration_max || undefined,
-  age_range_min: +queryParams.age_range_min || undefined,
-  age_range_max: +queryParams.age_range_max || undefined,
-});
+  return {
+    subject: queryParams.subject || undefined,
+    includeTags,
+    excludeTags,
+    duration_min: +queryParams.duration_min || undefined,
+    duration_max: +queryParams.duration_max || undefined,
+    age_range_min: +queryParams.age_range_min || undefined,
+    age_range_max: +queryParams.age_range_max || undefined,
+  };
+};
 
 export const dispatchSearchActions = (store: Store<RouterState>) => {
   const { router } = store.getState();
@@ -33,21 +31,10 @@ export const dispatchSearchActions = (store: Store<RouterState>) => {
     const query = queryParams.q as string;
     const page = parseInt(queryParams.page as string, 10);
 
-    const userFilters = getFilters(queryParams);
-
+    const filters = getFilters(queryParams);
     const sortBy = null;
 
-    store.dispatch(
-      searchVideosAction({
-        query,
-        page,
-        filters: { ...defaultFilters, ...userFilters },
-        sortBy,
-      }),
-    );
-
-    if (every(userFilters, isUndefined)) {
-      store.dispatch(searchCollectionsAction({ query }));
-    }
+    store.dispatch(searchVideosAction({ query, page, filters, sortBy }));
+    store.dispatch(searchCollectionsAction({ query }));
   }
 };
