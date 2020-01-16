3c16e6af8ffb2de7242594eb04ca8d35828c5ed3
Alex Glen <alex@boclips.com>
2020-01-16 12:38:35 +0000

    Refactor and condense the VideoDetailsView tests
    [#169702223]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/views/videoDetails/VideoDetailsView.test.tsx
+++ src/views/videoDetails/VideoDetailsView.test.tsx
@@ -1,42 +1,44 @@
-import { createBrowserHistory, createMemoryHistory } from 'history';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import reduceReducers from 'reduce-reducers';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import { wait, waitForElement } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { waitForElement } from '@testing-library/react';
 import React from 'react';
+import { renderWithCreatedStore } from '../../../test-support/renderWithStore';
 import {
-  renderWithCreatedStore,
-  renderWithStore,
-} from '../../../test-support/renderWithStore';
-import {
-  CollectionsFactory,
-  EntitiesFactory,
   LinksFactory,
   MockStoreFactory,
-  RouterFactory,
   UserProfileFactory,
-  VideoFactory,
   VideoResourceFactory,
 } from '../../../test-support/factories';
 import { VideoDetailsPage } from '../../../test-support/page-objects/VideoDetailsPage';
 import ApiStub from '../../../test-support/ApiStub';
-import { videoHandlers } from '../../components/video/redux/reducers/videoReducer';
-import { createReducer } from '../../app/redux/createReducer';
 import State from '../../types/State';
 import { createBoclipsStore } from '../../app/redux/store';
+import eventually from '../../../test-support/eventually';
 import { VideoDetailsView } from './VideoDetailsView';
 
 describe('VideoDetailsView', () => {
-  const initialState = MockStoreFactory.sampleState({
-    links: LinksFactory.sample({}, 'https://api.example.com/v1'),
-  });
-
   const video = VideoResourceFactory.sample({
     title: 'Video Title To Show',
     id: '123',
   });
+
+  const createViewWrapper = (initialHistory, initialState) => {
+    const history = createMemoryHistory({
+      initialEntries: initialHistory,
+    });
+
+    const store = createBoclipsStore(history, {
+      ...MockStoreFactory.sampleState({
+        links: LinksFactory.sample({}, 'https://api.example.com/v1'),
+      }),
+      ...initialState,
+    });
+
+    return renderWithCreatedStore(
+      <VideoDetailsView videoId={video.id} />,
+      store,
+      history,
+    );
+  };
 
   beforeEach(() => {
     new ApiStub()
@@ -61,185 +63,98 @@ describe('VideoDetailsView', () => {
 
   describe('When unauthenticated', () => {
     const unauthenticatedState: Partial<State> = {
-      ...initialState,
       authentication: {
         status: 'anonymous',
       },
+      user: null,
     };
 
     it('asks for a code when "share" and "referer" params are present', () => {
-      const history = createMemoryHistory({
-        initialEntries: ['/videos/123?referer=user-123&share=true'],
-      });
-
-      const store = createStore(
-        reduceReducers(
-          combineReducers({ router: connectRouter(history) }),
-          createReducer(...videoHandlers),
-        ),
+      const { getByText, getByRole } = createViewWrapper(
+        ['/videos/123?referer=user-123&share=true'],
         unauthenticatedState,
-        applyMiddleware(routerMiddleware(history)),
-      );
-      const wrapper = renderWithCreatedStore(
-        <VideoDetailsView videoId="123" />,
-        store,
-        history,
       );
 
-      const button = wrapper.getByText('Watch video');
+      const button = getByText('Watch video');
 
-      expect(wrapper.getByRole('dialog')).toBeInTheDocument();
+      expect(getByRole('dialog')).toBeInTheDocument();
       expect(button).toBeInTheDocument();
-      expect(
-        wrapper.getByText('Enter code to watch video'),
-      ).toBeInTheDocument();
+      expect(getByText('Enter code to watch video')).toBeInTheDocument();
     });
 
     it('does not ask for a code when the "referer" param is missing', () => {
-      const wrapper = renderWithStore(<VideoDetailsView videoId={'123'} />, {
-        initialState: unauthenticatedState,
-      });
+      const { queryByRole, queryByText } = createViewWrapper(
+        ['/videos/123'],
+        unauthenticatedState,
+      );
 
-      expect(wrapper.queryByRole('dialog')).not.toBeInTheDocument();
-      expect(
-        wrapper.queryByText('Enter code to watch video'),
-      ).not.toBeInTheDocument();
+      expect(queryByRole('dialog')).not.toBeInTheDocument();
+      expect(queryByText('Enter code to watch video')).not.toBeInTheDocument();
     });
+
     it('does not ask for a code when the "share" param is missing', () => {
-      const wrapper = renderWithStore(<VideoDetailsView videoId={'123'} />, {
-        initialState: {
-          router: RouterFactory.sample({
-            location: {
-              search: '?referer=user-id',
-              pathname: '',
-              hash: '',
-              state: null,
-            },
-          }),
-          links: LinksFactory.sampleAnonymous(),
-          collections: CollectionsFactory.sample(),
-          entities: EntitiesFactory.sample({
-            videos: { byId: { '123': VideoFactory.sample() } },
-          }),
-          authentication: {
-            status: 'anonymous',
-          },
-        },
-      });
+      const { queryByText, queryByRole } = createViewWrapper(
+        ['/videos/123?referer=user-123'],
+        unauthenticatedState,
+      );
 
-      expect(wrapper.queryByRole('dialog')).not.toBeInTheDocument();
-      expect(
-        wrapper.queryByText('Enter code to watch video'),
-      ).not.toBeInTheDocument();
+      expect(queryByRole('dialog')).not.toBeInTheDocument();
+      expect(queryByText('Enter code to watch video')).not.toBeInTheDocument();
     });
+
     it('does not render a share button', () => {
-      const wrapper = renderWithStore(<VideoDetailsView videoId={'123'} />, {
-        initialState: {
-          router: RouterFactory.sample({
-            location: {
-              search: '',
-              pathname: '',
-              hash: '',
-              state: null,
-            },
-          }),
-          links: LinksFactory.sampleAnonymous('https://api.example.com/v1'),
-          collections: CollectionsFactory.sample(),
-          entities: EntitiesFactory.sample({
-            videos: { byId: { '123': VideoFactory.sample() } },
-          }),
-          authentication: {
-            status: 'anonymous',
-          },
-        },
-      });
+      const { queryByText } = createViewWrapper(
+        ['/videos/123'],
+        unauthenticatedState,
+      );
 
-      expect(wrapper.queryByText('Share')).not.toBeInTheDocument();
+      expect(queryByText('Share')).not.toBeInTheDocument();
     });
+
     it('does not update the "referer" query param if it is set', async () => {
-      const history = createMemoryHistory({});
-
-      const anonymousState: Partial<State> = {
-        ...initialState,
-        authentication: { status: 'anonymous' },
-        user: null,
-      };
-
-      history.push({
-        search: `?referer=user-id-1`,
-      });
-
-      const store = createBoclipsStore(history, anonymousState);
-
-      const context = renderWithCreatedStore(
-        <VideoDetailsView videoId={video.id} />,
-        store,
-        history,
+      const { findByText, history } = createViewWrapper(
+        ['/videos/123?referer=user-id-1'],
+        unauthenticatedState,
       );
 
-      await context.findByText(video.title);
+      await findByText(video.title);
 
-      await wait(() => {
-        console.log(history.location.search);
-        if (history.location.search !== '?referer=user-id-1') {
-          throw Error();
-        }
+      await eventually(() => {
+        expect(history.location.search).toContain('referer=user-id-1');
       });
     });
+
     it('adds an anonymous "referer" query param if it was missing', async () => {
-      const history = createMemoryHistory({});
-
-      const anonymousState: Partial<State> = {
-        ...initialState,
-        authentication: { status: 'anonymous' },
-      };
-
-      const store = createBoclipsStore(history, anonymousState);
-
-      const context = renderWithCreatedStore(
-        <VideoDetailsView videoId={video.id} />,
-        store,
-        history,
+      const { findByText, history } = createViewWrapper(
+        ['/videos/123'],
+        unauthenticatedState,
       );
 
-      await context.findByText(video.title);
+      await findByText(video.title);
 
-      await wait(() => {
-        if (history.location.search.match(/referer=anonymous/)) {
-          throw Error();
-        }
+      await eventually(() => {
+        expect(history.location.search).toContain('referer=anonymous');
       });
     });
   });
 
   describe('When authenticated', () => {
-    it.todo('does render a share button');
     const authenticatedState = {
-      ...initialState,
       authenticated: { status: 'authenticated' },
       user: UserProfileFactory.sample({ id: 'user-test-id' }),
     };
-
     it('does not ask for a code', async () => {
-      const history = createMemoryHistory({
-        initialEntries: ['/videos/123?referer=user-123&share=true'],
-      });
-
-      const store = createBoclipsStore(history, authenticatedState);
-
-      const wrapper = renderWithCreatedStore(
-        <VideoDetailsView videoId={video.id} />,
-        store,
-        history,
+      const { findByText, getByText } = createViewWrapper(
+        ['/videos/123?referer=user-123&share=true'],
+        authenticatedState,
       );
 
-      const title = await wrapper.findByText(video.title);
+      expect(await findByText(video.title)).toBeInTheDocument();
 
-      expect(title).toBeInTheDocument();
       await expect(
         waitForElement(
           () => {
-            wrapper.getByText('Watch video');
+            getByText('Watch video');
           },
           {
             timeout: (2 * 60) / 1000,
@@ -249,47 +164,32 @@ describe('VideoDetailsView', () => {
     });
 
     it('sets the "referer" query param', async () => {
-      const history = createBrowserHistory();
-
-      const store = createBoclipsStore(history, authenticatedState);
-
-      const context = renderWithStore(
-        <Provider store={store}>
-          <Router history={history}>
-            <VideoDetailsView videoId={video.id} />
-          </Router>
-        </Provider>,
+      const { findByText, history } = createViewWrapper(
+        ['/videos/123'],
+        authenticatedState,
       );
 
-      await context.findByText(video.title);
+      await findByText(video.title);
 
-      await wait(() => {
-        if (history.location.search !== '?referer=user-test-id') {
-          throw Error();
-        }
+      await eventually(() => {
+        expect(history.location.search).toContain(
+          `referer=${authenticatedState.user.id}`,
+        );
       });
     });
+
     it('sets overwrites the "referer" query param if it is already set', async () => {
-      const history = createMemoryHistory({});
-
-      history.push({
-        search: `?referer=user-id-1`,
-      });
-
-      const store = createBoclipsStore(history, authenticatedState);
-
-      const context = renderWithCreatedStore(
-        <VideoDetailsView videoId={video.id} />,
-        store,
-        history,
+      const { findByText, history } = createViewWrapper(
+        ['/videos/123?referer=user-id-1'],
+        authenticatedState,
       );
 
-      await context.findByText(video.title);
+      await findByText(video.title);
 
-      await wait(() => {
-        if (history.location.search !== '?referer=user-test-id') {
-          throw Error();
-        }
+      await eventually(() => {
+        expect(history.location.search).toContain(
+          `referer=${authenticatedState.user.id}`,
+        );
       });
     });
   });
