7a4314619692e106120beb56f13e7f96133218d9
Rebecca Appleyard <rebecca@boclips.com>
2020-01-21 18:48:46 +0000

    Delete unused preview components
    [#169680516]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/components/collection/card/preview/EmptyCollectionVideoCardPreview.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EmptyCollectionCardVideoPreview = React.memo(() => (
-  <section className="collection-card-video-container">
-    <section className="collection-card-video-container__inner collection-card-video-container--blue" />
-  </section>
-));
-
-export default EmptyCollectionCardVideoPreview;
