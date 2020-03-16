70c7d35a8ed6f8c7c4de2885abe22641c9eafdff
Eva Varga <eva@boclips.com>
2020-03-16 16:58:51 +0000

    Highlight most common countries in onboarding
    [#171673419]

---
+++ src/components/account/form/CountriesForm.test.tsx
@@ -0,0 +1,59 @@
+import {
+  CountriesForm,
+  CountriesFormProps,
+} from 'src/components/account/form/CountriesForm';
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { CountryFactory } from 'test-support/factories';
+import { Form } from 'antd';
+import { FormComponentProps } from 'antd/es/form';
+
+describe(`CountriesForm`, () => {
+  const CountriesFormWithMockedForm = Form.create<
+    CountriesFormProps & FormComponentProps
+  >()(CountriesForm);
+
+  it(`renders highlighted countries first, then sort the rest by abc`, async () => {
+    const testCountries = [
+      CountryFactory.sample({ id: 'GBR', name: 'United Kingdom' }),
+      CountryFactory.sample({ id: 'URY', name: 'Uruguay' }),
+      CountryFactory.sample({ id: 'USA', name: 'United States' }),
+      CountryFactory.sample({ id: 'DZA', name: 'Algeria' }),
+      CountryFactory.sample({ id: 'HUN', name: 'Hungary' }),
+      CountryFactory.sample({ id: 'TON', name: 'Tonga' }),
+      CountryFactory.sample({ id: 'BEL', name: 'Belgium' }),
+      CountryFactory.sample({ id: 'IND', name: 'India' }),
+      CountryFactory.sample({ id: 'UZB', name: 'Uzbekistan' }),
+      CountryFactory.sample({ id: 'CAN', name: 'Canada' }),
+      CountryFactory.sample({ id: 'AUS', name: 'Australia' }),
+    ];
+
+    const expectedOrder = [
+      'United States',
+      'United Kingdom',
+      'Australia',
+      'Canada',
+      'India',
+      'Algeria',
+      'Belgium',
+      'Hungary',
+      'Tonga',
+      'Uruguay',
+      'Uzbekistan',
+    ];
+
+    const component = render(
+      <CountriesFormWithMockedForm
+        countries={testCountries}
+        placeholder={'Choose country'}
+      />,
+    );
+
+    fireEvent.click(component.getByText('Choose country'));
+    const options = await component.findAllByRole('option');
+
+    expectedOrder.forEach((countryName, index) => {
+      expect(options[index].title).toEqual(countryName);
+    });
+  });
+});
--- src/components/account/form/CountriesForm.tsx
+++ src/components/account/form/CountriesForm.tsx
@@ -4,14 +4,17 @@ import React from 'react';
 import { Country } from '../../../types/Country';
 import MultiSelect from '../../common/MultiSelect';
 import '../../common/MultiSelect.less';
+import './CountriesForm.less';
 
-interface CountriesFormProps {
+export interface CountriesFormProps {
   countries: Country[];
   placeholder: string;
   label?: string;
   initialValue?: string;
   onCountryChange?: (value: Country) => void;
 }
+
+export const HIGHLIGHTED_COUNTRY_IDS = ['USA', 'GBR', 'AUS', 'CAN', 'IND'];
 
 export class CountriesForm extends React.Component<
   FormComponentProps & CountriesFormProps
@@ -58,16 +61,47 @@ export class CountriesForm extends React.Component<
 
   private generateOptions() {
     const Option = MultiSelect.Option;
+    const orderedCountries: Country[] = [];
+    const lastHighlightedCountryId =
+      HIGHLIGHTED_COUNTRY_IDS[HIGHLIGHTED_COUNTRY_IDS.length - 1];
 
-    return (
-      this.props.countries &&
-      this.props.countries
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map(country => (
-          <Option key={country.name} value={country.id} title={country.name}>
-            {country.name}
-          </Option>
-        ))
-    );
+    if (this.props.countries) {
+      const sortedCountries = this.props.countries.sort((a, b) =>
+        a.name.localeCompare(b.name),
+      );
+
+      const countriesWithoutHighlights: Country[] = [];
+      const countriesHighlighted: Country[] = [];
+
+      sortedCountries.forEach(country => {
+        if (HIGHLIGHTED_COUNTRY_IDS.indexOf(country.id) > -1) {
+          countriesHighlighted.push(country);
+        } else {
+          countriesWithoutHighlights.push(country);
+        }
+      });
+
+      const countriesHighlightedOrdered = countriesHighlighted.sort((a, b) =>
+        HIGHLIGHTED_COUNTRY_IDS.indexOf(a.id) >
+        HIGHLIGHTED_COUNTRY_IDS.indexOf(b.id)
+          ? 1
+          : -1,
+      );
+
+      orderedCountries.push(...countriesHighlightedOrdered);
+      orderedCountries.push(...countriesWithoutHighlights);
+    }
+    return orderedCountries?.map(country => (
+      <Option
+        key={country.name}
+        value={country.id}
+        title={country.name}
+        className={
+          country.id === lastHighlightedCountryId ? 'highlight-border' : ''
+        }
+      >
+        {country.name}
+      </Option>
+    ));
   }
 }
