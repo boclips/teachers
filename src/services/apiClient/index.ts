68920ddad3727b6133d8a1a577c0d42d6f2c278c
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-05-01 13:12:38 +0100

    Do not instantiate Boclips Api Client mutliple times
    [#172328258]

--- src/services/apiClient/index.ts
+++ src/services/apiClient/index.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 import { ApiBoclipsClient, BoclipsClient } from 'boclips-api-client';
 import { Constants } from '../../app/AppConstants';
 
-export const getBoclipsClient = (): Promise<BoclipsClient> =>
-  ApiBoclipsClient.create(axios, Constants.API_PREFIX);
+const client = ApiBoclipsClient.create(axios, Constants.API_PREFIX);
+
+export const getBoclipsClient = (): Promise<BoclipsClient> => client;
