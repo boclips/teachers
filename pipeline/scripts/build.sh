e101a9e18954758e8a2fbb7082520872796b8c14
Boisababy Clips <bob@fleming.nowhere>
2020-05-26 15:51:43 +0200

    remove {} from var

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -8,7 +8,7 @@ export SENTRY_RELEASE
 echo "Adding bit.dev to npm registry"
 echo "always-auth=true" >> ~/.npmrc
 echo "@bit:registry=https://node.bit.dev" >> ~/.npmrc
-echo "//node.bit.dev/:_authToken={$BIT_TOKEN}" >> ~/.npmrc
+echo "//node.bit.dev/:_authToken=$BIT_TOKEN" >> ~/.npmrc
 echo "Completed adding bit.dev to npm registry"
 npm -v
 npm ci
