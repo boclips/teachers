ecc9f684e2fccf61507892f965ab2b12cc9a0659
Boisababy Clips <bob@fleming.nowhere>
2020-05-26 15:54:50 +0200

    add .npmrc

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -5,11 +5,6 @@ set -x -e
 SENTRY_RELEASE="teachers-$(cat ../version/tag)"
 export SENTRY_RELEASE
 
-echo "Adding bit.dev to npm registry"
-echo "always-auth=true" >> ~/.npmrc
-echo "@bit:registry=https://node.bit.dev" >> ~/.npmrc
-echo "//node.bit.dev/:_authToken=$BIT_TOKEN" >> ~/.npmrc
-echo "Completed adding bit.dev to npm registry"
 npm -v
 npm ci
 npm run build:ci
