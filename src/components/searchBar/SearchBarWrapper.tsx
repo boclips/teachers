b60d4e039422ef76156548e281dd89d3dae0ad9c
Alex Glen <alex@boclips.com>
2020-07-09 10:41:46 +0100

    Fix search bar query persisting between screens
    [#171673201]
    
    Signed-off-by: Mima Farley-Rose <mima@boclips.com>

--- src/components/searchBar/SearchBarWrapper.tsx
+++ src/components/searchBar/SearchBarWrapper.tsx
@@ -1,44 +1,32 @@
 import queryString from 'query-string';
 import React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SearchBar from '@bit/dev-boclips.boclips-ui.components.search-bar';
 import { RouterState } from '../../types/State';
 import { bulkUpdateSearchParamsAction } from '../searchResults/redux/actions/updateSearchParametersActions';
 import './SearchBarWrapper.less';
 
-interface StateProps {
-  query?: string;
-}
-
-interface DispatchProps {
-  onQuerySubmitted: (query: string) => void;
-}
-
-export const SearchBarWrapper = (props: StateProps & DispatchProps) => {
+export const SearchBarWrapper = () => {
+  const dispatch = useDispatch();
+  const query = useSelector(
+    (state: RouterState) =>
+      queryString.parse(state.router.location.search).q as string,
+  );
+  const onQuerySubmitted = (search: string) => {
+    dispatch(bulkUpdateSearchParamsAction([{ page: 1 }, { q: search }]));
+    console.log('current query is ', search);
+  };
   return (
     <div className="search-bar">
       <SearchBar
         size="large"
+        initialQuery={query}
+        key={query}
         placeholder="Enter your search term"
-        onSearch={props.onQuerySubmitted}
+        onSearch={onQuerySubmitted}
       />
     </div>
   );
 };
 
-function mapStateToProps(state: RouterState): StateProps {
-  return {
-    query: queryString.parse(state.router.location.search).q as string,
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
-  return {
-    onQuerySubmitted: (query: string) => {
-      dispatch(bulkUpdateSearchParamsAction([{ page: 1 }, { q: query }]));
-    },
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBarWrapper);
+export default SearchBarWrapper;
