bf851175fe4e51a09f34b5d146d44d91a4f3f2c7
Remigiusz Oleszczuk <remy@boclips.com>
2020-07-28 13:05:29 +0200

    Remove bit form teachers

--- src/views/bit/index.tsx
+++ src/views/bit/index.tsx
@@ -1,69 +1,10 @@
 /* eslint-disable */
-import React, { useEffect } from 'react';
-import Rating from 'src/components/video/rating/Rating';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchVideosAction } from 'src/components/video/redux/actions/fetchVideosAction';
-import VideoPlayer from 'src/components/video/player/VideoPlayer';
-import VideoButtons from 'src/components/video/buttons/videoButtons/VideoButtons';
-import { VideoCard } from '@bit/boclips.boclips-ui.components.video-card';
-import AnalyticsFactory from 'src/services/analytics/AnalyticsFactory';
-import { Video } from 'src/types/Video';
-import State from 'src/types/State';
+import React from 'react';
 
 export const Bit = (): any => {
-  const dispatch = useDispatch();
-  // @ts-ignore
-  const videos = useSelector((state) => state.entities.videos.byId);
-
-  // @ts-ignore
-  useEffect(() => {
-    dispatch(
-      fetchVideosAction({
-        filters: {
-          type: ['STOCK', 'INSTRUCTIONAL'],
-          duration: null,
-          age_range: null,
-        },
-        sortBy: null,
-        query: 'math',
-        page: 1,
-      }),
-    );
-  }, []);
-
-  const emitVideoLinkClickEvent = (video: Video) => {
-    AnalyticsFactory.internalAnalytics()
-      .trackVideoLinkClicked(video)
-      .then((r) => r);
-  };
-
-  const isAuthenticated = useSelector((state: State) => !!state.user);
-
   return (
     <div style={{ padding: '50px', marginTop: '100px', marginBottom: '100px' }}>
-      {Object.keys(videos).map((it, i) => {
-        const video = videos[it];
-        return (
-          <>
-            <VideoCard
-              video={video}
-              rating={<Rating video={video} />}
-              authenticated={isAuthenticated}
-              videoPlayer={
-                <VideoPlayer
-                  video={video}
-                  videoIndex={i}
-                  mode="card"
-                  collectionKey="discoverCollections"
-                />
-              }
-              videoActionButtons={<VideoButtons video={video} mode="card" />}
-              analytics={() => emitVideoLinkClickEvent(video)}
-              key={video.id}
-            />
-          </>
-        );
-      })}
+      bit goes here
     </div>
   );
 };
