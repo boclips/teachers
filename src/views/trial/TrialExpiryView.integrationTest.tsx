def33a9d2caf4dacd9163d8a3c5525161b85d400
Eva Varga <eva@boclips.com>
2021-02-18 17:56:27 +0000

    Upgrade to api-client 31.*
    [#176996938]

--- src/views/trial/TrialExpiryView.integrationTest.tsx
+++ src/views/trial/TrialExpiryView.integrationTest.tsx
@@ -43,7 +43,7 @@ describe('TrialExpiryView', () => {
 
     const client = (await ApiClientWrapper.get()) as FakeBoclipsClient;
 
-    expect(client.events.getEvents()).toContain('USER_EXPIRED');
+    expect(client.events.getEvents()[0].type).toEqual('USER_EXPIRED');
   });
 
   it('does not tell the user the trial is over, when it is not over', async () => {
